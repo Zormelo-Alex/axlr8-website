a9ff3f58d076a002aafe3b76205cae5dc2e2f3a4
Zormelo_Alex <alexzormelo9@gmail.com>
2022-12-24 05:36:01 -0800

    finished the footer and burger

--- src/partials/footer.js
+++ src/partials/footer.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faFacebookF, faInstagram, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+import htulogo from "../resources/images/htulogo.jfif";
 
 const footer = () => {
   return (
     <div className="footer">
         <h2>AXLR8</h2>
-        <p>Dot dot dot dot</p>
+        <p><img src={htulogo} alt="" /></p>
         <div className="links">
           <ul>
-            <li><a href="">something</a></li>
-            <li><a href="">something</a></li>
-            <li><a href="">something</a></li>
-            <li><a href="">something</a></li>
-            <li><a href="">something</a></li>
-            <li><a href="">something</a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
           </ul>
         </div>
         <div className="social-links">
           <ul>
-            <li><a href=""><FontAwesomeIcon icon={faFacebookF}></FontAwesomeIcon></a></li>
-            <li><a href=""><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a></li>
-            <li><a href=""><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a></li>
-            <li><a href=""><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></a></li>
-            <li><a href=""><FontAwesomeIcon icon={faLinkedinIn}></FontAwesomeIcon></a></li>
+            <li><a href="#"><FontAwesomeIcon icon={faFacebookF}></FontAwesomeIcon></a></li>
+            <li><a href="#"><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a></li>
+            <li><a href="#"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a></li>
+            <li><a href="#"><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></a></li>
+            <li><a href="#"><FontAwesomeIcon icon={faLinkedinIn}></FontAwesomeIcon></a></li>
           </ul>
         </div>
         <div className="text">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat eos ullam, non nulla laboriosam vitae nisi harum, asperiores incidunt cumque nam recusandae at neque ipsa, accusamus qui quisquam impedit. Corporis.</p>
+          <p>copyright &copy; 2023 AXLR8 | All rights reserved</p>
         </div>
       </div>
   )
