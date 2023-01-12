42f3702d2f0757b4be9cd8f7052ae8b5fca07c21
Zormelo_Alex <alexzormelo9@gmail.com>
2023-01-11 18:37:24 -0800

    removed the preloader

--- src/partials/frontPage.js
+++ src/partials/frontPage.js
@@ -16,83 +16,74 @@ const burger = () => {
     document.querySelector(".frontpage").classList.remove("active");
     document.querySelector(".c").classList.remove("no-scroll")
   }
-  window.addEventListener("load", ()=>{
-    document.querySelector(".yin-yang-container").style.display = "none";
-  })
 const frontPage = () => {
   return (
-    <>
-      <div className="yin-yang-container">
-        <div className="yin-yang"></div>
-        <div className="name">AXLR8</div>
-      </div>
-      <div className="frontpage">
-        <div className="navbar">
-          <a href="#">
-            <div className="logo">
-              <img src={logo} alt="axlr8 logo" />
-              <p>AXLR8</p>
-            </div>
-          </a>
-          <div className="navlinks">
-            <ul>
-              <li onClick={ lis }><a href="#about">About</a></li>
-              <li onClick={ lis }><a href="#services">Services</a></li>
-              <li onClick={ lis }><a href="#events">Events</a></li>
-              <li onClick={ lis }><a href="#contact">Contact Us</a></li>
-            </ul>
+    <div className="frontpage">
+      <div className="navbar">
+        <a href="#">
+          <div className="logo">
+            <img src={logo} alt="axlr8 logo" />
+            <p>AXLR8</p>
           </div>
-          <div onClick={burger} className="burger">
-            <div className="line"></div>
-            <div className="line two"></div>
-            <div className="line three"></div>
-          </div>
+        </a>
+        <div className="navlinks">
+          <ul>
+            <li onClick={ lis }><a href="#about">About</a></li>
+            <li onClick={ lis }><a href="#services">Services</a></li>
+            <li onClick={ lis }><a href="#events">Events</a></li>
+            <li onClick={ lis }><a href="#contact">Contact Us</a></li>
+          </ul>
         </div>
-        <div className="flex">
-          <div className="left">
-            <div className="slogan" data-aos="fade-right">
-              <h4>Wherever you go, <br /> Go with all <br /> your heart.</  h4>
-            </div>
-            <div className="card" data-aos="fade-left">
-              <div className="item">
-                <div className="image">
-                  <img src={olc} alt="" />
-                </div>
-                <div className="text">
-                  <h4>Online Courses</h4>
-                  <p>Learn something new today!</p>
-                </div>
-                <div className="bottom">View</div>
-              </div>
-              <div className="item">
-                <div className="image">
-                  <img src={merch} alt="" />
-                </div>
-                <div className="text">
-                  <h4>Merchandise</h4>
-                  <p>Order something fashionable now!</p>
-                </div>
-                <div className="bottom">shop</div>
-              </div>
-            </div>
-          </div>
-          <div className="right" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
-            <div className="video">
-              <div className="circle top"></div>
-              <div className="circle side"></div>
-              <div className="circle"></div>
-              <video>
-              {/* autoPlay muted loop */}
-                <source src={video1} type="video/mp4"/>
-              </video>
-            </div>
-            <div className="image one"><img src={com} alt="" /></div>
-            <div className="image two"><img src={cc} alt="" /></div>
-            <div className="image three"><img src={aa} alt="" /></div>
-          </div>
+        <div onClick={burger} className="burger">
+          <div className="line"></div>
+          <div className="line two"></div>
+          <div className="line three"></div>
         </div>
       </div>
-    </>
+      <div className="flex">
+        <div className="left">
+          <div className="slogan" data-aos="fade-right">
+            <h4>Wherever you go, <br /> Go with all <br /> your heart.</  h4>
+          </div>
+          <div className="card" data-aos="fade-left">
+            <div className="item">
+              <div className="image">
+                <img src={olc} alt="img" loading='lazy'/>
+              </div>
+              <div className="text">
+                <h4>Online Courses</h4>
+                <p>Learn something new today!</p>
+              </div>
+              <div className="bottom">View</div>
+            </div>
+            <div className="item">
+              <div className="image">
+                <img src={merch} alt="img" loading='lazy'/>
+              </div>
+              <div className="text">
+                <h4>Merchandise</h4>
+                <p>Order something fashionable now!</p>
+              </div>
+              <div className="bottom">shop</div>
+            </div>
+          </div>
+        </div>
+        <div className="right" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+          <div className="video">
+            <div className="circle top"></div>
+            <div className="circle side"></div>
+            <div className="circle"></div>
+            <video>
+            {/* autoPlay muted loop */}
+              <source src={video1} type="video/mp4"/>
+            </video>
+          </div>
+          <div className="image one"><img src={com} alt="" loading='lazy'/></div>
+          <div className="image two"><img src={cc} alt="" loading='lazy' /></div>
+          <div className="image three"><img src={aa} alt="" loading='lazy'/></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
